9d7ba0dc4f0c7e19b84705ed4384aec7a9774263
rahulvarma722 <rahulvarma722@gmail.com>
2024-05-16 12:16:27 +0530

    emotion css added

--- dist/components.js
+++ dist/components.js
@@ -1,15 +1,27 @@
 import * as React from 'react';
+import { css, cx } from '@emotion/css';
 
 const Button = ({ children, onClick }) => {
-    return (React.createElement("button", { onClick: onClick, style: {
-            padding: "10px 20px",
-            fontSize: "1.2em",
-            borderRadius: "5px",
-            cursor: "pointer",
-            backgroundColor: "blue",
-            color: "white",
-            border: "none",
-        } }, children));
+    const color = "darkgreen";
+    css `
+  &.extra-class {
+    background-color: hotpink;
+  }
+  &:hover {
+    color: ${color};
+  }
+`;
+    const cls1 = css `
+  font-size: 20px;
+  background: green;
+`;
+    const cls2 = css `
+  font-size: 20px;
+  background: blue;
+`;
+    return (React.createElement(React.Fragment, null,
+        React.createElement("button", { onClick: onClick, className: cx(cls1, cls2) }, children),
+        React.createElement("button", { onClick: onClick, className: cx(cls1, cls2) }, children)));
 };
 
 export { Button };
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,26 +1,47 @@
 import * as React from "react";
 import { ReactNode } from "react";
+import { cx, css } from '@emotion/css'
 
 type PropsType = {
   children: ReactNode;
   onClick?: () => void;
 };
 const Button = ({ children, onClick }: PropsType) => {
+  const color = "darkgreen";
+
+  const buttonCss = css`
+  &.extra-class {
+    background-color: hotpink;
+  }
+  &:hover {
+    color: ${color};
+  }
+`;
+
+const cls1 = css`
+  font-size: 20px;
+  background: green;
+`;
+const cls2 = css`
+  font-size: 20px;
+  background: blue;
+`;
+
   return (
-    <button
-      onClick={onClick}
-      style={{
-        padding: "10px 20px",
-        fontSize: "1.2em",
-        borderRadius: "5px",
-        cursor: "pointer",
-        backgroundColor: "blue",
-        color: "white",
-        border: "none",
-      }}
-    >
-      {children}
-    </button>
+    <>
+      <button
+        onClick={onClick}
+        className={cx(cls1, cls2)}
+      >
+        {children}
+      </button>
+      <button
+        onClick={onClick}
+        className={cx(cls1, cls2)}
+      >
+        {children}
+      </button>
+    </>
   );
 };
 
