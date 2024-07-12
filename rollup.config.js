5aa961db53ec38b2e33b07ce18bc27466d148e0f
Jaied Al Sabid <87969327+jaieds@users.noreply.github.com>
2024-07-12 11:55:11 +0600

    fix: CSS not loading from 3rd party libraries
    after import

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,61 @@
 import { defineConfig } from "rollup";
 import typescript from "@rollup/plugin-typescript";
+import postcss from "rollup-plugin-postcss";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
 export default defineConfig([
-  {
-    input: "src/index.ts",
-    output: {
-      dir: "dist",
-      format: "es",
-      name: "bsf-admin-ui",
+    {
+        input: "src/index.ts",
+        output: {
+            dir: "dist",
+            format: "es",
+            name: "bsf-admin-ui",
+        },
+        external: ["react", "react-dom", "@emotion/css"],
+        plugins: [
+            typescript({
+                tsconfig: "tsconfig.json",
+            }),
+        ],
     },
-    external: ["react", "react-dom", "@emotion/css"],
-    plugins: [
-      typescript({
-        tsconfig: "tsconfig.json",
-      }),
-    ],
-  },
-  {
-    input: "src/components.ts",
-    output: {
-      dir: "dist",
-      format: "es",
-      name: "bsf-admin-ui",
+    {
+        input: "src/components.ts",
+        output: {
+            dir: "dist",
+            format: "es",
+            name: "bsf-admin-ui",
+        },
+        external: ["react", "react-dom", "@emotion/css", "quill", "quill-mention"],
+        plugins: [
+            typescript({
+                tsconfig: "tsconfig.json",
+            }),
+            resolve(),
+            commonjs({
+              namedExports: {
+                'hoist-non-react-statics': ['default'] // Explicitly specify the default export for hoist-non-react-statics
+              }
+            }),
+            postcss({
+              inject: true, // Injects CSS into the JS bundle
+              minimize: true, // Minimizes CSS
+              extensions: [".css"],
+            })
+        ],
     },
-    external: ["react", "react-dom", "@emotion/css"],
-    plugins: [
-      typescript({
-        tsconfig: "tsconfig.json",
-      }),
-    ],
-  },
-  {
-    input: "src/settings.ts",
-    output: {
-      dir: "dist",
-      format: "es",
-      name: "bsf-admin-ui",
+    {
+        input: "src/settings.ts",
+        output: {
+            dir: "dist",
+            format: "es",
+            name: "bsf-admin-ui",
+        },
+        external: ["react", "react-dom", "@emotion/css"],
+        plugins: [
+            typescript({
+                tsconfig: "tsconfig.json",
+            }),
+        ],
     },
-    external: ["react", "react-dom", "@emotion/css"],
-    plugins: [
-      typescript({
-        tsconfig: "tsconfig.json",
-      }),
-    ],
-  },
 ]);
