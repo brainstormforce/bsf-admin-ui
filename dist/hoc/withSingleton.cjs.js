11cc6e65d46e8d11b7a3c2cb59646677c25d69e2
Navanath Bhosale <navanathb@bsf.io>
2026-02-09 19:20:29 +0530

    Test dist deletion

--- dist/hoc/withSingleton.cjs.js
+++
@@ -1,2 +0,0 @@
-"use strict";Object.defineProperties(exports,{__esModule:{value:!0},[Symbol.toStringTag]:{value:"Module"}});const r=require("react/jsx-runtime"),i=require("react");let n=!1;const l=t=>i.memo(e=>{const o=e.singleTon;return n&&o?null:(n=!0,r.jsx(t,{...e}))});exports.default=l;
-//# sourceMappingURL=withSingleton.cjs.js.map
